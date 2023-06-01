ee7a747380ab8f5a6d7db65429ef8ab34733952f
Tetiana Blinova <tiana002@gmail.com>
2023-06-01 23:20:17 +0300

    add hover

--- src/components/OptionsFeedback/OptionsFeedback.styled.jsx
+++ src/components/OptionsFeedback/OptionsFeedback.styled.jsx
@@ -19,5 +19,12 @@ export const Btn = styled.button`
 	color: #1c1010;
   background-color: #e9dfdf;
   box-shadow: 0px 2px 4px rgba(179, 21, 21, 0.56), 0px 2px 4px rgba(236, 171, 171, 0.06),
-    1px 4px 6px rgba(81, 51, 51, 0.16);
+    1px 4px 6px;
+
+  &:hover,
+  &:focus {
+    color: white;
+    background-color: rgba(179, 21, 21, 0.56);
+    cursor: pointer;
+  }
 `;
